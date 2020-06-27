b93c92877869286885232ab5d838b0166fd1e6b0
eames-palmer <jpalmer4@uw.edu>
2020-06-27 14:44:47 -0700

    dropdown added

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const path = require('path');
+const { isOptionalChain } = require('typescript');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,9 +20,10 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('languagehelper.start', function() {
+	let disposable = vscode.commands.registerCommand('languagehelper.start',
+		() => {
 
-        supportedLanguages = ['Python'];
+        supportedLanguages = ['Python', 'C', 'JavaScript'];
 
         const panel = vscode.window.createWebviewPanel(
             'lngHelper',
@@ -48,14 +50,25 @@ function activate(context) {
     context.subscriptions.push(disposable);
 }
 
-// function selectPage(supportedLanguages) {
-// 	let welcomePage = ;
-// 	return welcomePage;
-// }
+function selectLanguage(supportedLanguages) {
+	//populate dropdown
+	for (let i = 0; i < supportedLanguages.length; i++) {
+		let dropdown = document.getElementById('languages');
+		dropdown.createElement('option');
+		option.text = supportedLanguages[i];
+		dropdown.add(option);
+	}
+
+	//redirect to challenge
+	document.getElementById('go').addEventListener('click', () => {
+
+	});
+}
+
 
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
+// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
